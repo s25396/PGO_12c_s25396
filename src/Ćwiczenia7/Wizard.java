a08bfec785d8be5c8b4cc129202ce6cda762b27d
s25396 <s25396@pjwstk.edu.pl>
2022-06-25 14:14:44 +0200

    sd

--- src/Ćwiczenia7/Wizard.java
+++ src/Ćwiczenia7/Wizard.java
@@ -17,24 +17,25 @@ public class Wizard extends Character{
     }
 
     public void castSpell(Spell spell) {
-        if (spell == Spell.AvadaKedavra){
-            for (Character whoDie : targets) {
-                if (whoDie.getName().equals("Harry")){
-                    System.out.println(whoDie.getName()+" goes LOL...");
-                    this.setHealth(0);
-                }
-                else {
-                    if(this instanceof Voldemort){
-                        whoDie.setHealth(0);
-                        System.out.println(whoDie.getName()+" died.");
+            if (spell == Spell.AvadaKedavra)
+            {
+                for (Character whoDie : targets) {
+                        if (whoDie.getName().equals("Harry")) {
+                            System.out.println(whoDie.getName() + " goes LOL...");
+                            this.setHealth(0);
+                        }
+                        else if(this instanceof Voldemort) {
+                            whoDie.setHealth(0);
+                            System.out.println(whoDie.getName() + " died.");
+                        }
+
                     }
 
+                if (this instanceof Voldemort)
+                {
+                    System.out.println(this.getName() + " died.");
                 }
-
-            }
-            if(this instanceof Voldemort){
-                System.out.println(this.getName()+ " died.");
+                targets = null;
             }
         }
     }
-}
